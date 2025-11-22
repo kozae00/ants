c6ecfcd901f4d78e74be0fdede5d81a0b7897577
kozae00 <rhwogus777@gmail.com>
2025-11-23 02:25:42 +0900

    init

---
+++ backend/src/test/java/com/kozae/ants/AntsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kozae.ants;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
