3d1ca5509f45a76e719de58c3f5e84a18a672b11
kozae00 <rhwogus777@gmail.com>
2025-11-24 15:20:30 +0900

    refactor: spring ver 4 -> 3

--- backend/src/main/java/com/kozae/ants/global/config/SecurityConfig.java
+++ backend/src/main/java/com/kozae/ants/global/config/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/members/signup", "/api/members/login").permitAll()
+                        .requestMatchers("/api/v1/members/signup", "/api/v1/members/login").permitAll()
                         .requestMatchers("/h2-console/**").permitAll()
                         .anyRequest().authenticated()
                 )
